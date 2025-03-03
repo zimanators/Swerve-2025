c81b2b67df285b763c87498c190da78192b94ba3
zimanators <106321671+zimanators@users.noreply.github.com>
2025-03-03 10:48:05 -0500

    Update arm.java
    updated the buttons

--- src/main/java/frc/robot/subsystems/arm.java
+++ src/main/java/frc/robot/subsystems/arm.java
@@ -1,19 +1,14 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.RelativeEncoder; // Import the encoder library
+import com.revrobotics.spark.SparkMax; // Updated import paths
+import com.revrobotics.spark.SparkLowLevel.MotorType; // Updated import paths
+import com.revrobotics.AbsoluteEncoder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.ControlType;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.AbsoluteEncoder;
 
 public class arm extends TimedRobot {
-    private final CANSparkMax leftMotor = new CANSparkMax(1, MotorType.kBrushless);
-    private final CANSparkMax rightMotor = new CANSparkMax(2, MotorType.kBrushless);
+    private final SparkMax leftMotor = new SparkMax(9, MotorType.kBrushless); // Updated class name
+    private final SparkMax rightMotor = new SparkMax(10, MotorType.kBrushless); // Updated class name
     private final XboxController controller = new XboxController(0);
     private final AbsoluteEncoder encoder = leftMotor.getAbsoluteEncoder(); // Initialize the absolute encoder
 
@@ -25,7 +20,7 @@ public class arm extends TimedRobot {
     public void teleopPeriodic() {
         double currentAngle = encoder.getPosition(); // Get the current position in degrees
 
-        if (controller.getPOV() == 0) { // D-pad up
+        if (controller.getXButton()) { // X button
             if (currentAngle < TARGET_ANGLE_1) {
                 leftMotor.set(1.0); // Forward
                 rightMotor.set(1.0); // Forward
@@ -33,7 +28,7 @@ public class arm extends TimedRobot {
                 leftMotor.set(0.0); // Stop
                 rightMotor.set(0.0); // Stop
             }
-        } else if (controller.getPOV() == 180) { // D-pad down
+        } else if (controller.getBButton()) { // B button
             if (currentAngle > -TARGET_ANGLE_2) {
                 leftMotor.set(-1.0); // Backward
                 rightMotor.set(-1.0); // Backward
